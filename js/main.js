c769b161069c96795a241b5855c38a22aeb6a98a
Sarbajit Paul <sarbajitpaul1996@gmail.com>
2017-07-11 01:06:11 +0530

    Added some payment functionality.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+$(document).ready(function() {
+    $('select').material_select();
+  });
+
 $(function(){
   $('#filled-in-box').change(function(){
     $('.collapsible').slideToggle(500);
+  });
+
+  $('.datepicker').pickadate({
+    //selectMonths: true, // Creates a dropdown to control month
+    //selectYears: 15 // Creates a dropdown of 15 years to control year
   });
 });
